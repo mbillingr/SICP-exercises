2e4feeb955c25d299f60c1e0a0b51fd06052f629
Martin Billinger <flkazemakase@gmail.com>
2019-03-25 23:21:35 +0100

    use block structure

--- chapter1/exercise1-08.scm
+++ chapter1/exercise1-08.scm
@@ -1,27 +1,28 @@
 #!/usr/bin/env -S guile -s
 !#
 
-(define (cube-root x) (curt-iter 1.0 x))
+(define (cube-root x) 
+    (define (curt-iter guess)
+        (define improved-guess (improve guess))
+        (if (good-enough? guess improved-guess)
+            improved-guess
+            (curt-iter improved-guess)))
 
-(define (curt-iter guess x)
-    (define improved-guess (improve guess x))
-    (if (good-enough? guess improved-guess)
-        improved-guess
-        (curt-iter improved-guess x)))
+    (define (improve y)
+        (/ (+ (/ x (sqr y))
+              (* 2 y))
+           3))
 
-(define (improve y x)
-    (/ (+ (/ x (sqr y))
-          (* 2 y))
-       3))
+    (define (average a b)
+        (/ (+ a b)
+           2))
 
-(define (average a b)
-    (/ (+ a b)
-       2))
+    (define (good-enough? guess new-guess)
+        (< (abs (/ (- guess new-guess)
+                   guess))
+           1e-12))
 
-(define (good-enough? guess new-guess)
-    (< (abs (/ (- guess new-guess)
-               guess))
-       1e-12))
+    (curt-iter 1.0))
 
 (define (sqr x) (* x x))
 (define (cube x) (* x x x))
