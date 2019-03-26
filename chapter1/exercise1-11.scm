2f87f32cdd1a380f3871c65634dfbe1d5dae97b5
Martin Billinger <flkazemakase@gmail.com>
2019-03-26 22:11:43 +0100

    exercise 1.11

---
+++ chapter1/exercise1-11.scm
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (f-recursive n)
+    (if (< n 3)
+        n
+        (+ (f-recursive (- n 1))
+           (* 2 (f-recursive (- n 2)))
+           (* 3 (f-recursive (- n 3))))))
+
+(define (f-iterative a b c n)
+    (if (= n 0)
+        c
+        (f-iterative (+ a (* 2 b) (* 3 c))
+                     a b (- n 1))))
+
+(define (f n) (f-iterative 2 1 0 n))
+
+(display (f 30))
+(newline)
+(display (f-recursive 30))
