1b1e314e9ff2fa4a955a924147b4774cdff2e017
Martin Billinger <flkazemakase@gmail.com>
2019-03-26 22:28:41 +0100

    exercise 1.12

---
+++ chapter1/exercise1-12.scm
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (triangle row col)
+    (cond ((= 0 col) 1)
+          ((= row col) 1)
+          (else (+ (triangle (- row 1) 
+                             (- col 1))
+                   (triangle (- row 1) 
+                             col)))))
+
+(define (show-row row col)
+    (display (triangle row col))
+    (if (= row col)
+        -1
+        (show-row row (+ 1 col))))
+
+(define (show-triangle row)
+    (show-row row 0)
+    (newline)
+    (if (= row 0)
+        -1
+        (show-triangle (- row 1))))
+        
+(show-triangle 4)
