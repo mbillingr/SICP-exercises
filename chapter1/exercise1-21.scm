a05cf574ed6669a80955af8b69c176407f3e74d6
Martin Billinger <flkazemakase@gmail.com>
2019-03-27 20:17:33 +0100

    exercise 1.21

---
+++ chapter1/exercise1-21.scm
@@ -0,0 +1,21 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (smallest-divisor n) (find-divisor n 2))
+
+(define (find-divisor n test-divisor)
+    (cond ((> (square test-divisor) n) n)
+          ((divides? test-divisor n) test-divisor)
+          (else (find-divisor n (+ test-divisor 1)))))
+
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (square x) (* x x))
+
+(define (prime? n) (= n (smallest-divisor n)))
+
+(display (smallest-divisor 199))
+(newline)
+(display (smallest-divisor 1999))
+(newline)
+(display (smallest-divisor 19999))
