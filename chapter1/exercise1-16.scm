88ee8cdcf4bb4b5dfd2aec181985adb3cc6bdf3f
mbillingr <mbillingr@users.noreply.github.com>
2019-03-27 15:35:08 +0100

    Create exercise1-16.scm

---
+++ chapter1/exercise1-16.scm
@@ -0,0 +1,10 @@
+; iterative exponentiation in O(log n) time
+
+(define (exp-iter a b n) 
+  (cond ((= n 0) a)
+        ((even? n) (exp-iter a (* b b) (/ n 2)))
+        (else (exp-iter (* a b) b (- n 1)))))
+		 
+(define (exp base exponent) (exp-iter 1 base exponent))
+		
+(exp 3 6)
