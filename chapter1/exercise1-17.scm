ee87a6988d9e772ae06c652e681b14500d94a7b4
mbillingr <mbillingr@users.noreply.github.com>
2019-03-27 15:46:36 +0100

    Create exercise1-17.scm

---
+++ chapter1/exercise1-17.scm
@@ -0,0 +1,12 @@
+(define (mul-iter a x y) 
+	(cond ((= y 0) a)
+	      ((even? y) (mul-iter a (double x) (halve y)))
+		  (else (mul-iter (+ a x) x (- y 1)))))
+		  
+		  
+(define (double x) (+ x x))
+(define (halve x) (/ x 2))
+		 
+(define (mul x y) (mul-iter 0 x y))
+		
+(mul 3 6)
