0719d822e9b3057b9fd1bdfbe3f3a6b12c04432f
Martin <flkazemakase@gmail.com>
2019-04-02 12:57:53 +0200

    fix exercise 1.17

--- chapter1/exercise1-17.scm
+++ chapter1/exercise1-17.scm
@@ -4,7 +4,7 @@
 (define (mul x y)
   (cond ((= x 0) 0)
         ((= y 0) 0)
-        ((even? y) (mul (double x)) (halve y))
+        ((even? y) (mul (double x) (halve y)))
         (else (+ x (mul x (- y 1))))))
 
 (define (double x) (+ x x))
