ce207267414791bbd9adea650d2cf37252828158
Martin <flkazemakase@gmail.com>
2019-04-04 17:12:53 +0200

    exercise 1.37

---
+++ chapter1/exercise1-37.scm
@@ -0,0 +1,31 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (cont-frac-recursive n d k)
+  (define (recurse i)
+    (if (> i k)
+        0
+        (/ (n i)
+           (+ (d i) (recurse (+ i 1))))))
+  (recurse 1))
+
+(define (cont-frac n d k)
+  (define (iterate i result)
+    (if (= i 0)
+        result
+        (iterate (- i 1)
+                 (/ (n i)
+                    (+ result (d i))))))
+  (iterate k 0))
+
+(display "expected: 0.6180\n")
+
+(display (cont-frac-recursive (lambda (i) 1.0)
+                              (lambda (i) 1.0)
+                              12))
+(newline)
+
+(display (cont-frac (lambda (i) 1.0)
+                    (lambda (i) 1.0)
+                    12))
+(newline)
