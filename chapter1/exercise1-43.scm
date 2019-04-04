6dbfa389c00c88194be61b8fc9e015b77f803d62
Martin <flkazemakase@gmail.com>
2019-04-04 18:33:26 +0200

    exercise 1.43

---
+++ chapter1/exercise1-43.scm
@@ -0,0 +1,22 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (compose f g)
+  (lambda (x) (f (g x))))
+
+; recursive
+(define (repeated f n)
+  (if (= n 1)
+      (lambda (x) (f x))
+      (compose f (repeated f (- n 1)))))
+
+; iterative
+(define (repeated f n)
+  (define (iter i result)
+    (if (= i 1)
+        result
+        (iter (- i 1) (compose f result))))
+  (iter n f))
+
+(display ((repeated sqr 2) 5))
+(newline)
