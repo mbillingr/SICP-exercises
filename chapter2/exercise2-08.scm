f9050ead9201fb7f27732601328efd4d4a5efd14
Martin Billinger <flkazemakase@gmail.com>
2019-04-05 23:50:20 +0200

    exercise 2.8

---
+++ chapter2/exercise2-08.scm
@@ -0,0 +1,43 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x (make-interval (/ 1.0 (upper-bound y))
+                                 (/ 1.0 (lower-bound y)))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+
+(define a (make-interval 9 11))
+(define b (make-interval 3 8))
+(define c (make-interval 41.5 42.5))
+
+(display (add-interval a b))
+(newline)
+
+(display (mul-interval a b))
+(newline)
+
+(display (div-interval c a))
+(newline)
+
+(display (sub-interval a b))
+(newline)
