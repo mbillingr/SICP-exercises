d4c0e0b762807764169c20209313c749483e64c5
Martin Billinger <flkazemakase@gmail.com>
2019-04-06 00:21:08 +0200

    exercise 2.10

---
+++ chapter2/exercise2-10.scm
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (let ((ly (lower-bound y))
+        (uy (upper-bound y)))
+    (if (xor (< ly 0) (< uy 0))
+        (error "divide by zero-spanning interval")
+        (mul-interval x (make-interval (/ 1.0 (upper-bound y))
+                                       (/ 1.0 (lower-bound y)))))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+
+(define a (make-interval 9 11))
+(define b (make-interval 3 8))
+(define c (make-interval 41.5 42.5))
+(define d (make-interval -2 5))
+
+(display (add-interval a b))
+(newline)
+
+(display (mul-interval a b))
+(newline)
+
+(display (div-interval c a))
+(newline)
+
+(display (div-interval c d))
+(newline)
+
+(display (sub-interval a b))
+(newline)
