17c58c3bdf2b6a40fa5d2cc9ef09b511ea1f9ca1
Martin Billinger <flkazemakase@gmail.com>
2019-04-06 00:29:48 +0200

    exercise 2.12

---
+++ chapter2/exercise2-12.scm
@@ -0,0 +1,51 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x (make-interval (/ 1.0 (upper-bound y))
+                                 (/ 1.0 (lower-bound y)))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (make-interval a b) (cons a b))
+(define (lower-bound x) (car x))
+(define (upper-bound x) (cdr x))
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (make-center-width c (* c (/ p 100))))
+(define (percent i)
+  (* 100 (/ (width i) (center i))))
+
+
+(define a (make-interval 9 11))
+(define b (make-center-percent 50 20))
+
+(display (percent a))
+(newline)
+
+(display b)
+(newline)
+
+(display (percent b))
+(newline)
