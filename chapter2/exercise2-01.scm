342c07f03f604a9574d6cd154a3767f51c68c30e
Martin <flkazemakase@gmail.com>
2019-04-05 09:37:38 +0200

    exercise 2.1

---
+++ chapter2/exercise2-01.scm
@@ -0,0 +1,57 @@
+#!/usr/bin/env -S guile -s
+!#
+
+; rational arithmetic
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer y))))
+
+(define (equal-rat x y)
+  (= (* (numer x) (denom y))
+     (* (numer y) (denom x))))
+
+
+(define (numer x) (car x))
+(define (denom x) (cdr x))
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (let ((n (/ n g))
+          (d (/ d g)))
+      (if (< d 0)
+          (cons (- n) (- d))
+          (cons n d)))))
+
+(define (print-rat x)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(print-rat (make-rat 2 3)) (newline)
+(print-rat (make-rat -2 3)) (newline)
+(print-rat (make-rat 2 -3)) (newline)
+(print-rat (make-rat -2 -3)) (newline)
+
+(print-rat (make-rat 2 8)) (newline)
+(print-rat (make-rat -2 8)) (newline)
+(print-rat (make-rat 2 -8)) (newline)
+(print-rat (make-rat -2 -8)) (newline)
+
+(print-rat (make-rat 5 3)) (newline)
+(print-rat (make-rat -5 3)) (newline)
+(print-rat (make-rat 5 -3)) (newline)
+(print-rat (make-rat -5 -3)) (newline)
