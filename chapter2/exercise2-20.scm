00f3dc49ab998132f849048a0c5066d079e6b01a
Martin Billinger <flkazemakase@gmail.com>
2019-04-09 00:29:26 +0200

    exercise 2.20

---
+++ chapter2/exercise2-20.scm
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (same-parity first . rest)
+  (define (iter in ref)
+    (cond ((null? in) (list))
+          ((= (even? (car in))
+              ref)
+           (cons (car in)
+                 (iter (cdr in) ref)))
+          (else (iter (cdr in) ref))))
+  (cons first
+        (iter rest (even? first))))
+
+(display (list 1 2 3 4 5 6 7 8)) (display " -> ") (display (same-parity 1 2 3 4 5 6 7 8)) (newline)
+(display (list 2 3 4 5 6 7 8)) (display " -> ") (display (same-parity 2 3 4 5 6 7 8)) (newline)
