73717fac028bac519924be58ee62979b0e4258e5
Martin Billinger <flkazemakase@gmail.com>
2019-04-08 22:48:16 +0200

    exercise 2.18

---
+++ chapter2/exercise2-18.scm
@@ -0,0 +1,18 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (reverse lst)
+  (define (iter in out)
+    (if (null? in)
+        out
+        (iter (cdr in)
+              (cons (car in) out))))
+  (iter lst (list)))
+
+(define a (list 1 2 3 4 5))
+(define b (list 2 3 5 7 11))
+(define c (list))
+
+(display a) (display " -> ") (display (reverse a)) (newline)
+(display b) (display " -> ") (display (reverse b)) (newline)
+(display c) (display " -> ") (display (reverse c)) (newline)
