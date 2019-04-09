017f19f08ccd33a95e0dfdd449ef3d4d5c333281
Martin <flkazemakase@gmail.com>
2019-04-09 13:33:06 +0200

    exercise 2.21

---
+++ chapter2/exercise2-21.scm
@@ -0,0 +1,25 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define numbers (list 1 2 3 4 5 6))
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (square-list items)
+  (if (null? items)
+      nil
+      (cons (sqr (car items))
+            (square-list (cdr items)))))
+
+(display (square-list numbers))
+(newline)
+
+(define (square-list items)
+  (map sqr items))
+
+(display (square-list numbers))
+(newline)
