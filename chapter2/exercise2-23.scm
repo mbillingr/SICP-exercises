95bbd9d587cea51f0309a2e751f9ae59f6caebd6
Martin <flkazemakase@gmail.com>
2019-04-09 13:49:55 +0200

    exercise 2.23

---
+++ chapter2/exercise2-23.scm
@@ -0,0 +1,16 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define numbers (list 1 2 3 4 5 6))
+
+(define (for-each proc items)
+  (if (null? items)
+      nil
+      (begin (proc (car items))
+             (for-each proc (cdr items)))))
+
+
+(for-each (lambda (x)
+            (display x)
+            (newline))
+          (list 57 321 88))
