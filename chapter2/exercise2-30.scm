056709be430e4479cb60bfbf628f779f6ca03c59
Martin <flkazemakase@gmail.com>
2019-04-09 17:10:26 +0200

    exercise 2.30

---
+++ chapter2/exercise2-30.scm
@@ -0,0 +1,42 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define numbers (list 1 2 3 4 5 6))
+
+(define (map proc items)
+  (if (null? items)
+      nil
+      (cons (proc (car items))
+            (map proc (cdr items)))))
+
+(define (square-tree tree)
+  (map (lambda (subtree)
+         (if (pair? subtree)
+             (square-tree subtree)
+             (sqr subtree)))
+       tree))
+
+(display "expected: (1 (4 (9 16) 25) (36 49))\n")
+
+(display "  actual: ")
+(display (square-tree
+           (list 1
+                 (list 2 (list 3 4) 5)
+                 (list 6 7))))
+(newline)
+
+
+(define (square-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (sqr tree))
+        (else (cons (square-tree (car tree))
+                    (square-tree (cdr tree))))))
+
+(display "expected: (1 (4 (9 16) 25) (36 49))\n")
+
+(display "  actual: ")
+(display (square-tree
+           (list 1
+                (list 2 (list 3 4) 5)
+                (list 6 7))))
+(newline)
