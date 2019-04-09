52ffd46653d1e2c8dcbc8c1205a06e73b616220a
Martin <flkazemakase@gmail.com>
2019-04-09 16:00:12 +0200

    exercise 2.28

---
+++ chapter2/exercise2-28.scm
@@ -0,0 +1,49 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define (reverse lst)
+  (define (iter in out)
+    (if (null? in)
+        out
+        (iter (cdr in)
+              (cons (car in) out))))
+  (iter lst nil))
+
+; recursive implementation
+(define (fringe1 tree)
+  (cond ((null? tree) nil)
+        ((pair? tree) (append (fringe (car tree))
+                              (fringe (cdr tree))))
+        (else (list tree))))
+
+; reversed partially iterative implementation
+(define (fringe2 tree)
+  (define (iter tree out)
+    (cond ((null? tree) out)
+          ((pair? tree) (iter (cdr tree)
+                              (iter (car tree) out)))
+          (else (cons tree out))))
+  (reverse (iter tree nil)))
+
+(define fringe fringe2)
+
+(define x (list (list 1 2) (list 3 4)))
+(display (fringe x)) (newline)
+(display (fringe (list x x))) (newline)
+
+(display "\nperformance on tiny tree\n")
+(display "  recursive: ")
+(timeit (lambda () (fringe1 x)))
+(display "  iterative: ")
+(timeit (lambda () (fringe2 x)))
+
+
+(define a (list 1 2 3 4 5 6 7 8 9 10))
+(define b (list a a a a a a a a a a))
+(define c (list b b b b b b b b b b))
+
+(display "\nperformance on huge tree\n")
+(display "  recursive: ")
+(timeit (lambda () (fringe1 c)))
+(display "  iterative: ")
+(timeit (lambda () (fringe2 c)))
