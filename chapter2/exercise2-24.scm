dd454234e91d25438f594a2fedde85bfdcf194fb
Martin <flkazemakase@gmail.com>
2019-04-09 15:01:49 +0200

    exercise 2.24

---
+++ chapter2/exercise2-24.scm
@@ -0,0 +1,40 @@
+#!/usr/bin/env -S guile -s
+!#
+
+(define data (list 1 (list 2 (list 3 4))))
+
+; result printed by interpreter
+; expected: (1 (2 (3 4)))
+(display data) (newline)
+
+; box and pointer structure
+;
+; [.  .]
+;  |  |
+;  1  [.  0]
+;      |
+;     [.  .]
+;      |  |
+;      2  [.  0]
+;          |
+;          [.  .]
+;           |  |
+;           3  [.  0]
+;               |
+;               4
+
+; interpretation as tree
+;
+; (1 (2 (3 4)))
+; |
+; +-- 1
+; |
+; +-- (2 (3 4)))
+;     |
+;     +-- 2
+;     |
+;     +-- (3 4)
+;         |
+;         +-- 3
+;         |
+;         +-- 4
