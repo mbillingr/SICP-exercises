7de047d4820c6e57526a5ca0f00fd902b3876120
Martin <flkazemakase@gmail.com>
2019-04-12 11:21:50 +0200

    fix exercise 2.56

--- chapter2/exercise2-56.scm
+++ chapter2/exercise2-56.scm
@@ -37,8 +37,8 @@
 (define (make-sum a1 a2)
   (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
-        ((and (number? a1) (number? a2) (+ a1 a2)))
-        (else (list '+ a1 a2'))))
+        ((and (number? a1) (number? a2)) (+ a1 a2))
+        (else (list '+ a1 a2))))
 
 (define (make-product m1 m2)
   (cond ((or (=number? m1 0) (=number? m2 0)) 0)
