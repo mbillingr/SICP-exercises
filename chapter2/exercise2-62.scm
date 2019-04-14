3706c92f850c954cf7879b9e95f920347432146a
Martin Billinger <flkazemakase@gmail.com>
2019-04-14 20:05:57 +0200

    exercise 2.62

---
+++ chapter2/exercise2-62.scm
@@ -0,0 +1,57 @@
+#!/usr/bin/env -S guile -s
+!#
+
+; Sets as unordered lists
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (car set)) true)
+        ((< x (car set)) false)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set set1 set2)
+  (if (or (null? set1) (null? set2))
+      '()
+      (let ((x1 (car set1))
+            (x2 (car set2)))
+        (cond ((= x1 x2)
+               (cons x1
+                     (intersection-set (cdr set1)
+                                       (cdr set2))))
+              ((< x1 x2)
+               (intersection-set (cdr set1) set2))
+              ((< x2 x1)
+               (intersection-set set1 (cdr set2)))))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((= x (car set)) set)
+        ((< x (car set)) (cons x set))
+        (else (cons (car set)
+                    (adjoin-set x (cdr set))))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        (else
+          (let ((x1 (car set1))
+                (x2 (car set2)))
+            (cond ((= x1 x2)
+                   (cons x1
+                         (union-set (cdr set1)
+                                    (cdr set2))))
+                  ((< x1 x2)
+                   (cons x1 (union-set (cdr set1) set2)))
+                  ((< x2 x1)
+                   (cons x2 (union-set set1 (cdr set2)))))))))
+
+(define a '(1 2 3 4))
+(define b '(3 4 5 6))
+
+(println (intersection-set a b))
+
+(println (adjoin-set 3 a))
+(println (adjoin-set 7 a))
+(println (adjoin-set 6 (adjoin-set 7 a)))
+(println (union-set b a))
+(println (union-set a b))
