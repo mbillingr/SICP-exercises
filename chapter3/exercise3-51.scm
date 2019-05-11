ee11cf6b4d4ab42d65a7bd48a3f779151989e529
Martin Billinger <flkazemakase@gmail.com>
2019-05-11 22:53:32 +0200

    exercise 3.51

---
+++ chapter3/exercise3-51.scm
@@ -0,0 +1,56 @@
+(import (builtin core)
+        (sicp utils)
+        (sicp utils prime))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+        low
+        (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (println x))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+(define (show x)
+  (display-line x)
+  x)
+
+; this should print 0 ... the first item in the stream
+(define x (stream-map show (stream-enumerate-interval 0 10)))
+
+; this should print 1 2 3 4 5
+(stream-ref x 5)
+
+; and this should print 6 7
+(stream-ref x 7)
