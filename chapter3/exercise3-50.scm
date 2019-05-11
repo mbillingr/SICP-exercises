3ab5f0934c6357c6ad2089aa24e435548bb7e456
Martin Billinger <flkazemakase@gmail.com>
2019-05-11 22:49:17 +0200

    exercise 3.50

---
+++ chapter3/exercise3-50.scm
@@ -0,0 +1,52 @@
+(import (builtin core)
+        (sicp utils)
+        (sicp utils prime))
+
+(define (stream-ref s n)
+  (if (= n 0)
+      (stream-car s)
+      (stream-ref (stream-cdr s) (- n 1))))
+
+(define (stream-for-each proc s)
+  (if (stream-null? s)
+      'done
+      (begin (proc (stream-car s))
+             (stream-for-each proc (stream-cdr s)))))
+
+(define (stream-enumerate-interval low high)
+  (if (> low high)
+      the-empty-stream
+      (cons-stream
+        low
+        (stream-enumerate-interval (+ low 1) high))))
+
+(define (stream-filter pred stream)
+  (cond ((stream-null? stream) the-empty-stream)
+        ((pred (stream-car stream))
+         (cons-stream (stream-car stream)
+                      (stream-filter pred
+                                     (stream-cdr stream))))
+        (else (stream-filter pred (stream-cdr stream)))))
+
+(define (display-stream s)
+  (stream-for-each display-line s))
+
+(define (display-line x)
+  (println x))
+
+(define x (cons-stream 1
+            (cons-stream 2
+              (cons-stream 3
+                (cons-stream 4
+                  (cons-stream 5
+                    the-empty-stream))))))
+
+(define (stream-map proc . argstreams)
+  (if (stream-null? (car argstreams))
+      the-empty-stream
+      (cons-stream
+        (apply proc (map stream-car argstreams))
+        (apply stream-map
+               (cons proc (map stream-cdr argstreams))))))
+
+(display-stream (stream-map + x (stream-map sqr x)))
