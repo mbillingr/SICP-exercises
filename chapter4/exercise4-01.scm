afa44084b3a059cfe457c9b22fa44a95a1b47897
Martin <flkazemakase@gmail.com>
2019-05-17 11:11:42 +0200

    exercise 4.01

--- chapter4/exercise4-01.scm
+++ chapter4/exercise4-01.scm
@@ -8,7 +8,8 @@
 (define rest-operands cdr)
 
 (define (eval exp env)
-  (println "Evaluating" exp "in" env))
+  (println "Evaluating" exp "in" env)
+  exp)
 
 (define (list-of-values exps env)
   (if (no-operands? exps)
@@ -16,5 +17,31 @@
       (cons (eval (first-operand exps) env)
             (list-of-values (rest-operands exps) env))))
 
+(println (list-of-values '(1 2 3) '()))
 
-(list-of-values '(1 2 3) '())
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((left (eval (first-operand exps) env)))
+        (cons left
+              (list-of-values (rest-operands exps) env)))))
+
+(println (list-of-values '(1 2 3) '()))
+
+(define (list-of-values exps env)
+  (if (no-operands? exps)
+      '()
+      (let ((right (list-of-values (rest-operands exps) env)))
+        (cons (eval (first-operand exps) env)
+              right))))
+
+(println (list-of-values '(1 2 3) '()))
+
+; I verified that this works correctly by reversing the evaluation order
+; in the native interpreter:
+
+;     let args = (*cdr).map_list(|a| eval(a, env.clone()))?;
+; was replaced with
+;     let args: Vec<_> = cdr.iter_list().collect::<Result<_>>()?;
+;     let args: Vec<_> = args.into_iter().rev().map(|a| eval(a, env.clone())).collect::<Result<_>>()?;
+;     let args = args.into_iter().rev().collect()));
