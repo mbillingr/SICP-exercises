56cf8c7a57426a90d11336591512a34bcb57aa5c
Martin Billinger <flkazemakase@gmail.com>
2019-05-18 00:54:42 +0200

    exercise 4.02

--- chapter4/exercise4-02.scm
+++ chapter4/exercise4-02.scm
@@ -2,3 +2,30 @@
         (sicp utils))
 
 (include "chapter4-core.scm")
+
+(define (eval exp env)
+  (cond ((self-evaluating? exp) exp)
+        ((variable? exp) (lookup-variable-value exp env))
+        ((quoted? exp) (text-of-quotation exp))
+        ((application? exp)
+         (apply (eval (operator exp) env)
+                (list-of-values (operands exp) env)))
+        ((assignment? exp) (eval-assignment exp env))
+        ((definition? exp) (eval-definition exp env))
+        ((if? exp) (eval-if exp env))
+        ((lambda? exp)
+         (make-procedure (lambda-parameters exp)
+                         (lambda-body exp)
+                         env))
+        ((begin? exp) (eval-sequence (begin-actions exp) env))
+        ((cond? exp) (eval (cond->if exp) env))
+        (else (error "Unknown expression type -- EVAL" exp))))
+
+
+; a) This does not work because any pairs is considered an evaluation.
+;    The remaining special forms pass the (application?) test too.
+
+; b) changed syntax for calls
+(define (application? exp) (tagged-list? exp 'call))
+(define (operator exp) (cadr exp))
+(define (operands exp) (cddr exp))
