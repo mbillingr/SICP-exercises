12fa62d7c14f0d9c951c184e873713980a40f863
Martin Billinger <flkazemakase@gmail.com>
2019-05-23 00:58:50 +0200

    exercise 4.36

---
+++ chapter4/exercise4-36.scm
@@ -0,0 +1,20 @@
+(import (builtin core)
+        (sicp utils))
+
+(define (require p)
+  (if (not p) (amb)))
+
+(define (an-integer-starting-from n)
+  (amb n (an-integer-starting-from (+ n 1))))
+
+(define (an-integer-between lo hi)
+  (require (<= lo hi))
+  (amb lo (an-integer-between (+ lo 1) hi)))
+
+; can't test yet, so no attempt to optimize...
+(define (a-pythagorean-triple)
+  (let ((k (an-integer-starting-from 1)))
+    (let ((j (an-integer-between 1 k)))
+      (let ((i (an-integer-between 1 j)))
+        (require (= (+ (* i i) (* j j) (* k k))))
+        (list i j k)))))
