dc93f28f5e48def61c422320ae9cb90dceb9b684
Martin Billinger <flkazemakase@gmail.com>
2019-05-23 01:04:59 +0200

    exercise 4.37

---
+++ chapter4/exercise4-37.scm
@@ -0,0 +1,5 @@
+(import (builtin core)
+        (sicp utils))
+
+; The version in this exercise is almost certainly more efficient; at least
+; for large numbers because it has only two nested "loops" instead of three.
