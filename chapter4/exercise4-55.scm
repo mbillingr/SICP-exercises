1199d5cdaf7ec564f1127de33e1dc51feb827f38
Martin Billinger <flkazemakase@gmail.com>
2019-05-25 21:30:57 +0200

    exercise 4.55

---
+++ chapter4/exercise4-55.scm
@@ -0,0 +1,8 @@
+; a) all people supervised by Ben Bitdiddle
+'(supervisor ?x (Bitdiddle Ben))
+
+; b) the names and jobs of all people in the accounting division
+'(job ?name (accounting . ?job))
+
+; c) the names and and addresses of all people who live in Slumerville
+'(address ?name (Slumerville . ?address))
