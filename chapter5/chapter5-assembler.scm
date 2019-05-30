700a92a3d4c0a6edffeaaafa9fe7a46d6cb45e0d
Martin Billinger <flkazemakase@gmail.com>
2019-05-30 23:21:19 +0200

    fix

--- chapter5/chapter5-assembler.scm
+++ chapter5/chapter5-assembler.scm
@@ -126,11 +126,11 @@
 (define (make-save inst machine stack pc)
   (let ((reg (get-register machine (stack-inst-reg-name inst))))
     (lambda ()
-      (push-stack (get-contents reg))
+      (push stack (get-contents reg))
       (advance-pc pc))))
 
 (define (make-restore inst machine stack pc)
-  (let ((reg (get-register-machine (stack-inst-reg-name inst))))
+  (let ((reg (get-register machine (stack-inst-reg-name inst))))
     (lambda ()
       (set-contents! reg (pop stack))
       (advance-pc pc))))
